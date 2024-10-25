15178117a3e95e9e395ac21b90920d29b36e3c7b
rhqshipon <rhqshipon@gmail.com>
2024-10-25 15:29:38 +0600

    Initial commit

---
+++ Day 1/day_1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main()  {
+    cout << "Trust me, I really hate C++! Why not learn python instead?" << endl;
+    return 0;
+}
---
+++ Day 2/day_2.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+
+
+void zygote()    {
+    cout << "I really hate C++!" << endl; // But I love Pyhton hehe!
+    //return 0;
+}
+
+
+void about_myself() {
+    cout << "NAME: Mohammad Rabiul Hoque\nID: 24-59366-3\nEmail: public.rhqshipon@gmail.com\nHome Town: Madaripur" << endl;
+}
+
+
+void about_myself_fragmented() {
+    cout << "Name: Mohammad Rabiul Hoque" << endl;
+    cout << "ID: 24-59366-3" << endl;
+    cout << "Email: public.rhqshipon@gmail.com" << endl;
+    cout << "Home Town: Madaripur" << endl;
+}
+
+
+void about_myself_with_variables() {
+
+    //cout << "NAME: Mohammad Rabiul Hoque\nID: 24-59366-3\nEmail: public.rhqshipon@gmail.com\nHome Town: Madaripur" << endl;
+}
+
+
+void arithmaticx()   {
+    int a = 2;
+    int b = 5;
+    cout << "The difference between a & b = " << b - a << endl;
+}
+
+
+void user_input()   {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << "The value of a is : " << a << endl;
+    cout << "The value of b is : " << b << endl;
+    cout << "The summation of a and b is : " << a + b  << endl;
+}
+
+
+void user_input2()  {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << "The input for a is : " << a << endl;
+    cout << "The input for b is : " << b << endl;
+    cout << "The addition for a and b is : " << a + b << endl;
+    cout << "The substraction for a and b is : " << a - b << endl;
+}
+
+
+void user_inputx()   {
+    int a, b;
+    cin >> a;
+    cout << "The Provided Input is: " << a;
+}
+
+
+int main()    {
+    //about_myself();
+    //arithmatic();
+    //user_input();
+    user_input2();
+    //zygote();
+
+
+    //cout << " Hello WORLD!" << endl; // But I love Pyhton hehe!
+    return 68;
+}
