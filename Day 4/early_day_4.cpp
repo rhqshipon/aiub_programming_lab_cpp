62955dcf3cbebdb89970096f6e9c07e4c41c79a6
rhqshipon <rhqshipon@gmail.com>
2024-11-01 10:22:22 +0600

    added early contents of day 4

---
+++ Day 4/early_day_4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void zygote()    {
+
+}
+
+void check_integer_or_odd() {
+    int input;
+    cout << "Provide me a valur for 'a': ";
+    cin >> input;
+    if ( input % 2 == 0)    {
+        cout << "even" << endl;
+    }
+    else    {
+        cout << "odd" << endl;
+    }
+}
+
+void eligibility_for_vote()    {
+    int age;
+    cout << "Provide me your age: ";
+    cin >> age;
+    if( age >= 18)    {
+        cout << "Congratulations! You are eligible for vote :)" << endl;
+    }
+    else    {
+        cout << "Sorry! You are not eligible for vote :(" << endl;
+    }
+}
+
+int main()    {
+    //check_integer_or_odd();
+    eligibility_for_vote();
+    cout << "" << endl;
+}
+
+
+
