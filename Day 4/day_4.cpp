4023cf3f8d211116a5d13d95118ac85564fac022
rhqshipon <rhqshipon@gmail.com>
2024-11-05 19:27:46 +0600

    Add day 5

--- Day 4/day_4.cpp
+++ Day 4/day_4.cpp
@@ -5,9 +5,100 @@ void zygote()    {
 
 }
 
+void determine_area()   {
+    int choice;
+    float area, dimension1, dimension2;
+    cout << "What do you want to do?" << endl;
+    cout << "[1] Area of Circle" << endl;
+    cout << "[2] Area of Square" << endl;
+    cout << "[3] Area of Triangle" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    switch(choice)  {
+        case 1 :
+            cout << "Give me the radius: ";
+            cin >> dimension1;
+            area = 3.1416*dimension1*dimension1;
+            cout << "The area is: " << area << " square units" << endl;
+            break;
+        case 2 :
+            cout << "Give me the length of dimension: ";
+            cin >> dimension1;
+            area = dimension1*dimension1;
+            cout << "The area is: " << area << " square units" << endl;
+            break;
+        case 3 :
+            cout << "Give me the length of base: ";
+            cin >> dimension1;
+            cout << "Give me the length of height: ";
+            cin >> dimension2;
+            area = .5*dimension1*dimension2;
+            cout << "The area is: " << area << " square units" << endl;
+            break;
+        default:
+            cout << "Invalid response!" << endl;
+
+    }
+}
+
+void grade_message()    {
+    char verdict;
+    cin >> verdict;
+    switch(verdict)    {
+        case 'A' :
+        case 'a' :
+            cout << "Excellent!" << endl;
+            break;
+        case 'B' :
+        case 'b' :
+            cout << "Good!" << endl;
+            break;
+        case 'C' :
+        case 'c' :
+            cout << "Average!" << endl;
+            break;
+        case 'D' :
+        case 'd' :
+            cout << "Poor!" << endl;
+            break;
+        case 'F' :
+        case 'f' :
+            cout << "Fail!" << endl;
+            break;
+        default:
+            cout << "Invalid response!" << endl;
+    }
+}
+
+void leap_year()    {
+    int year;
+    cout << "Provide me the year you want to detect the leap day of: ";
+    cin >> year;
+    if (year % 4 == 0)    {
+        if (year % 100 == 0)   {
+            if(year % 400 == 0)    {
+                cout << "This is a leap year!";
+            }
+            else    {
+                cout << "This is not a leap year.";
+            }
+        }
+        else    {
+            cout << "This is a leap year!";
+        }
+    }
+    else    {
+            cout << "This is not a leap year!";
+    }
+}
+
+
+
 
 int main()    {
-
+    //grade_message();
+    leap_year();
+    //determine_area();
     cout << "" << endl;
 }
 
--- Day 4/early_day_4.cpp
+++ Day 4/early_day_4.cpp
@@ -98,11 +98,52 @@ void calculator()   {
     }
 }
 
+void calculator_new()   {
+    float value1, value2;
+    char operatorr;
+    cout << "Type in your 1st value: ";
+    cin >> value1;
+    cout << "Type in your 2nd value: ";
+    cin >> value2;
+    cout << "Type in your expression: " << endl;
+    cin >> operatorr;
+    switch(operatorr)    {
+        case '+' :
+            cout << "       Answer: " << value1 + value2 << endl;
+            break;
+        case '-' :
+            cout << "       Answer: " << value1 - value2 << endl;
+            break;
+        case '*' :
+            cout << "       Answer: " << value1 * value2 << endl;
+            break;
+        case '/' :
+            if( value2 != 0 )   {
+                cout << "       Answer: " << value1 / value2 << endl;
+            }
+            else    {
+                cout << "       Error: Division by zero is undefined." << endl;
+            }
+            break;
+        case '%' :
+            if( value2 != 0 )   {
+                cout << "       Answer: " << (int) value1 % (int) value2 << endl;
+            }
+            else    {
+                cout << "       Error: Modulo by zero is undefined." << endl;
+            }
+            break;
+        default :
+            cout << "   Error: Only +, -, *, /, % operations are supported!" << endl;
+    }
+}
+
 int main()    {
     //check_integer_or_odd();
     //check_if_leap_year();
     //eligibility_for_vote();
-    calculator();
+    //calculator();
+    calculator_new();
     cout << "" << endl;
 }
 
